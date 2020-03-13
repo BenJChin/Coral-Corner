2783e7da5fe8960f65e96cc468d558a18f51a555
BenC <benchin86@gmail.com>
2020-03-13 16:11:31 -0700

    made a login page and firebase api js

---
+++ scripts/firebase_api_coral.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDpy3lkqeJRgtMwF8ORVoOBMvM9C7ZLiYo",
+    authDomain: "coralcorner-b8d78.firebaseapp.com",
+    databaseURL: "https://coralcorner-b8d78.firebaseio.com",
+    projectId: "coralcorner-b8d78",
+    storageBucket: "coralcorner-b8d78.appspot.com",
+    messagingSenderId: "805148954178",
+    appId: "1:805148954178:web:d619c70e0e17fc72d3e7db",
+    measurementId: "G-3T26SZR4T2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
